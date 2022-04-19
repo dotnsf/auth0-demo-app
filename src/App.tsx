1b444e64d6a44b0d784b38ddea6280935978000a
K.Kimura <dotnsf@gmail.com>
2022-04-19 14:04:03 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
   return (
     <div className="App">
       <header className="App-header">
+        {!isAuthenticated ? (
+          <button onClick={loginWithRedirect}>Login</button>
+        ) : (
+          <button onClick={() => {
+            logout( { returnTo: window.location.origin } );
+          }}>Logout</button>
+        )}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
